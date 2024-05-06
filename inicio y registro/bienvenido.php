c36c66cb80abd2f6607f8839d2886a4a0a3c98c2
Arleondo2 <arnolsmith.tacca@ucsp.edu.pe>
2024-05-05 23:23:37 -0500

    Avance en la Integracion
    Bugs a arreglar:
    -Conexion del sesion_id para que funque
    -Agregar texto (se bugeo por eso lo quite)
    -Arreglar el funcionamiento de las tablas

--- inicio y registro/bienvenido.php
+++ inicio y registro/bienvenido.php
@@ -84,7 +84,49 @@
             </div>
 
         <div class="grid-item grid-PUBLICACIONES">
-            PUBLICACIONES
+
+            <form id="photos"   method="POST" enctype="multipart/form-data">
+
+                <label class="control-label" for="input01">Imagen:</label>
+
+                <input type="file" name="image" class="font" required>
+
+                <br><button type="submit" name="submit" class="btn btn-success"><i class="icon-upload"></i> Subir Foto</button>
+
+            </form>
+
+            <?php
+            if (isset($_POST['submit'])) {
+
+                $image = addslashes(file_get_contents($_FILES['image']['tmp_name']));
+                $image_name = addslashes($_FILES['image']['name']);
+                $image_size = getimagesize($_FILES['image']['tmp_name']);
+
+                move_uploaded_file($_FILES["image"]["tmp_name"], "upload/" . $_FILES["image"]["name"]);
+                $location = "upload/" . $_FILES["image"]["name"];
+                $conn->query("insert into photos (location,member_id) values ('$location','$session_id')");
+                ?>
+                <script>
+                    window.location = 'photos.php';
+                </script>
+                <?php
+            }
+            ?>
+
+            <hr>
+            <hr>
+            <?php
+            $query = $conn->query("select * from photos where member_id='$session_id'");
+            while($row = $query->fetch()){
+                $id = $row['photos_id'];
+                ?>
+                <div class="col-md-2 col-sm-3 text-center">
+                    <img class="photo" src="<?php echo $row['location']; ?>" >
+                    <hr>
+                    <a class="btn btn-danger" href="delete_photos.php<?php echo '?id='.$id; ?>"><i class="icon-remove"></i> Eliminar</a>
+                </div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <?php } ?>
+
         </div>
 
         <div class="grid-item grid-CHATS">
